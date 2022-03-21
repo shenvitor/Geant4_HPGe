9c00668ce1c7b3866a2e077a8a0d0aa110bac5ab
SVJ_Vitor <shenvitor@hotmail.com>
2022-03-22 06:13:30 +0800

    Update MaSAnalysis.h

--- MaSGe_Alcap/include/MaSAnalysis.h
+++ MaSGe_Alcap/include/MaSAnalysis.h
@@ -2,8 +2,9 @@
 #define MaSAnalysis_h 1
 
 #include "g4analysis_defs.hh"
+#include "g4root.hh"
 
-using namespace G4Csv;
-//using namespace G4Root;
+//using namespace G4Csv;
+using namespace G4Root;
 
 #endif
